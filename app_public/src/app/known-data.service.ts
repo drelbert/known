78e45cf2f31736d3d2e518d73d475fe2ce9b2cf8
drelbert <danrelbert@mail.com>
2019-01-28 14:46:36 -0500

    Updated known-data-service

--- app_public/src/app/known-data.service.ts
+++ app_public/src/app/known-data.service.ts
@@ -23,7 +23,7 @@ export class KnownDataService {
   constructor(private http: HttpClient) { }
 
   //the URL call
-  private apiBaseUrl = 'mongodb://eroku_bf9tx0rq:4o7hgbeftgukc62i3hb94mp6gl@ds163402.mlab.com:63402/heroku_bf9tx0rq';
+  private apiBaseUrl = 'mongodb://heroku_bf9tx0rq:4o7hgbeftgukc62i3hb94mp6gl@ds163402.mlab.com:63402/heroku_bf9tx0rq';
 
   //Below are data services which call the API.
   //Defining a method to get the projects for home-list and the details-page.
