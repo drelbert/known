b971064c86a7ac1ac56a941b8a98dbf5ae3beb46
drelbert <danrelbert@mail.com>
2019-01-28 14:56:38 -0500

    Updated app.js file for CORS

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(passport.initialize());
 
 //Allowing CORS req in Express 
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200','https://intense-river-13488.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200','https://intense-river-13488.herokuapp.com', 'mongodb://heroku_bf9tx0rq:4o7hgbeftgukc62i3hb94mp6gl@ds163402.mlab.com:63402/heroku_bf9tx0rq');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
  });
