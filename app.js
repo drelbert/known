8e319695ec75cfdb6e37d8c66f918859019834a3
drelbert <danrelbert@mail.com>
2019-03-12 17:27:23 -0400

    Updated CORS in app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
+  req.header('Access-Control-Allow-Origin', 'https://intense-river-13488.herokuapp.com');
  });
 
  //Error handlers to catch unauthorized errors.
