e90c3e56eff30e5baf8477fc69b732df2a94cc81
drelbert <danrelbert@mail.com>
2020-01-13 15:53:18 -0500

    adding the origninal mean known app

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'mongodb://heroku_bf9tx0rq:4o7hgbeftgukc62i3hb94mp6gl@ds163402.mlab.com:63402/heroku_bf9tx0rq'
+  apiBaseUrl: 'mongodb://:@ds163'
 };
